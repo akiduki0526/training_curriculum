339fdeb67748d3a6cdf0c0ef44f22aeae611ea7a
akiduki0526 <akiduki0526@gmail.com>
2022-12-08 15:51:20 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,7 +4,12 @@ class CalendarsController < ApplicationController
   def index
     getWeek
     @plan = Plan.new
+
   end
+
+
+
+
 
   # 予定の保存
   def create
@@ -15,7 +20,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
